0173b706d2931266ea065ec57a4e1733e63ac301
Hackbright Student <no-reply@hackbrightacademy.com>
2019-05-08 16:56:21 -0700

    login working

--- server.py
+++ server.py
@@ -39,7 +39,61 @@ def register_form():
 
     return render_template('register_form.html')
 
+@app.route('/register', methods=["POST"])
+def register_process():
+    """ Process registration """
 
+    email = request.form.get('email')
+    password = request.form.get('password')
+    age = request.form.get('age')
+    zipcode = request.form.get('zipcode')
+
+    user = User(email=email,
+                password=password,
+                age=age,
+                zipcode=zipcode)
+
+    db.session.add(user)
+    db.session.commit()
+
+    flash('Yay! Successfully added!')
+
+    return redirect('/')
+
+@app.route('/login')
+def login():
+    """Renders log in page"""
+
+    return render_template('login.html')
+
+@app.route('/logged-in')
+def check_logged_in():
+    """Check if use is in database and login if is"""
+
+    email = request.args.get("email")
+    password = request.args.get("password")
+
+    user_object = User.query.filter_by(email=email).first()
+
+    if user_object:
+        if user_object.password == password:
+            session["login"] = True
+            flash(f"Hey, welcome back {user_object.email} of zipcode {user_object.zipcode}")
+            return redirect("/")
+        else:
+            flash(f"""You're wrong. Are you even {user_object.email} of zipcode {user_object.zipcode}?
+                      The password is actually {user_object.password}""")
+            return redirect("/login")
+    else:
+        flash(f"""You're not real.""")
+        return redirect("/login")
+
+@app.route("/log-out")
+def log_out():
+    """Log user out"""
+    session["login"] = False
+    flash("Logged out")
+    return redirect("/")
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
